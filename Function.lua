cfb463e72a1a19676a2b689bc5c3b23805661a45
rollinsbaird <rmbaird@crimson.ua.edu>
2022-04-18 11:55:09 -0500

    small fix and comments to function

--- Function.lua
+++ Function.lua
@@ -1,3 +1,6 @@
+-- class that defines & calls functions
+-- Rollins Baird
+
 local Env = require 'Env'
 
 local function Function(node, env, is_init)
@@ -6,19 +9,21 @@ local function Function(node, env, is_init)
       return #node.parameters
     end,
 
+    -- runs function call
     call = function (interpret, arguments)
       local env = Env(env)
       for i = 1, #node.parameters do
         env.define(node.parameters[i].lexeme, arguments[i])
       end
       interpret(node.body, env)
-      if is_initalizer then return env.get_at(0, 'this') end
+      if is_init then return env.get_at(0, 'this') end
     end,
 
+    -- binds new function to enviroment
     bind = function (instance)
       local env = Env(env)
       env.define('this', instance)
-      return Function(node, env, is_initalizer)
+      return Function(node, env, is_init)
     end
   }, {
     __tostring = function ()
